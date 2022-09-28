223d059210b011b5d86c23b609c486c44fcef325
Harish30101999 <109505934+harish30101999@users.noreply.github.com>
2022-09-28 10:04:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpringBoot5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-aeex7z)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StudentComponent } from './student/student.component';
+// import { StudentComponent } from './book/book.component';
+// import { StudentComponent } from './student/student.component';
+@NgModule({
+  imports:      [ BrowserModule, FormsModule, HttpClientModule],
+  declarations: [ AppComponent, HelloComponent,StudentComponent ],
+  bootstrap:    [ StudentComponent]
+})
+export class AppModule { }
